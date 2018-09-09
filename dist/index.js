6b82dff4be8940274c72405c6daebe7aa8177361
Lee Brooks <leebrooks93@gmail.com>
2018-09-09 13:20:34 +0100

    Add things to export index

--- dist/index.js
+++ dist/index.js
@@ -3,12 +3,18 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 exports.__esModule = true;
+var AuthManager_1 = require("./app/Auth/AuthManager");
+exports.AuthManager = AuthManager_1["default"];
 var Event_1 = require("./app/Events/Event");
 exports.Event = Event_1["default"];
 var Job_1 = require("./app/Jobs/Job");
 exports.Job = Job_1["default"];
 var Listener_1 = require("./app/Listeners/Listener");
 exports.Listener = Listener_1["default"];
+var Route_1 = require("./app/Router/Route");
+exports.Route = Route_1["default"];
+var RouteManager_1 = require("./app/Router/RouteManager");
+exports.RouteManager = RouteManager_1["default"];
 var history_1 = require("./store/history");
 exports.history = history_1.history;
 __export(require("./store/middleware"));
